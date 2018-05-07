176ffcf3b18c7a4dc8ad868380ee8c4fb4b51013
Jake Romer <jake@jakeromer.com>
2018-05-07 19:45:04 -0400

    Add Buzzgo.elm, index.html, generate JS

---
+++ Buzzgo.elm
@@ -0,0 +1,42 @@
+module Buzzgo exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+
+
+playerInfo name gameNumber =
+    name ++ " -- Game #" ++ (toString gameNumber)
+
+
+viewPlayer name gameNumber =
+    let
+        playerInfoText =
+            playerInfo name gameNumber
+                |> String.toUpper
+                |> text
+    in
+        h2 [ id "info", class "classy" ] [ playerInfoText ]
+
+
+viewHeader title =
+    header []
+        [ h1 [] [ text title ] ]
+
+
+viewFooter =
+    footer []
+        [ a [ href "https://elm-lang-org" ]
+            [ text "Powered by Elm" ]
+        ]
+
+
+view =
+    div [ class "content" ]
+        [ viewHeader "Buzzword Bingo"
+        , viewPlayer "Nicole" 4
+        , viewFooter
+        ]
+
+
+main =
+    view
